4f63f288b3049be5857f1e4efc7ea92fa5b15e19
jklee <jkleeyt60@gmail.com>
2026-02-25 17:05:34 +0900

    Initial Commit

---
+++ cosign/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  cosign
+//
+//  Created by Lee MacMini on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ cosign/cosignApp.swift
@@ -0,0 +1,17 @@
+//
+//  cosignApp.swift
+//  cosign
+//
+//  Created by Lee MacMini on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct cosignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
